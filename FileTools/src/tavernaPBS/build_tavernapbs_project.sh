9dd81e3462d37fb81f2acfbb7495a850b08c728d
Simone Bnà <s.bn@cineca.it>
2016-03-04 15:37:37 +0000

    moved build_tavernapbs.sh to parent directory

--- FileTools/src/tavernaPBS/build_tavernapbs_project.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-# compile the java code
-javac -classpath ".:../../../Monitor/bin/lib/ganymed-ssh2-build250.jar" Config.java Extract.java Window.java  PBS.java Job.java PBSConvert.java Merge.java
-
-# create the jar archive
-jar cvf tavernaPBS.jar Config.class Extract.class Job.class Merge.class PBS.class PBSConvert.class Window.class ../../../Monitor/bin/lib/ganymed-ssh2-build250.jar
-
