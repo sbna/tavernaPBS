e5a37022fff64ed8178cc463bb79af5664835c9a
Simone Bnà <s.bn@cineca.it>
2016-03-11 12:33:26 +0100

    added CMakeLists and moved jar dependencies to externalJars folder

--- FileTools/src/build_tavernapbs_project.sh
+++ FileTools/src/build_tavernapbs_project.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 # compile the java code
 cd ./tavernaPBS
-javac -classpath ".:../ganymed-ssh2-build250.jar:../base64coder.jar" Config.java Extract.java Window.java  PBS.java Job.java PBSConvert.java Merge.java
+javac -classpath ".:../externalJars/ganymed-ssh2-build250.jar:../externalJars/base64coder.jar" Config.java Extract.java Window.java  PBS.java Job.java PBSConvert.java Merge.java
 cd ..
 
 # create the jar archive
